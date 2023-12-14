3d277bbcb04747c9c01629e54a6a727ea1e6d18c
AddanY <appdata.123@gmail.com>
2023-12-14 10:37:37 -0600

    Agregacion del input de numero telefonico

--- src/pages/ComponentsQuestions/FormularyContactForm.jsx
+++ src/pages/ComponentsQuestions/FormularyContactForm.jsx
@@ -1,23 +1,18 @@
 export const FormularyContactForm = () => {
   return (
-    <div className='container-contact-form'>
-        <div className='container-contact-header'>
-            <h2 className='container-contact-heading'>MANDA UN MENSAJE DIRECTO Y TE AYUDAREMOS CON TODAS TUS DUDAS</h2>
-        </div>
-        <form className="container-contact-form-body">
-            <div className='container-contact-field'>
-                <input className='container-contact-input-text' placeholder='Nombre' type="text" />
-            </div>
-            <div className='container-contact-field'>
-                <input className='container-contact-input-text' placeholder='Correo electronico' type="email" />
-            </div>
-            <div className='container-contact-field'>
-                <textarea className='container-contact-input-textarea' placeholder='Mensaje'></textarea>
-            </div>
-            <div className='container-contact-field'>
-                <input className='container-contact-input-submit' type="submit" value='Suscribete'/>
-            </div>
-        </form>
-    </div>
+    <div className="container-messageStraight">
+    <h4>
+        Manda un mensaje directo y te ayudaremos con todas tus dudas
+    </h4>
+
+    <form action="">
+      <input type="text" placeholder="Nombre"/>
+      <input type="email" placeholder="Correo electronico"/>
+      <input type="tel" placeholder="Números de teléfono"/>
+      <textarea name="textarea" rows="10" cols="50" placeholder="Mensaje"></textarea>
+
+      <button type="submit" className="sendBtnEditStyles"> Suscribete </button>
+    </form>
+  </div>
   )
 }
